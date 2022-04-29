5bbde58587c53e56f14c7aae6a30d3a094d411e1
Khalil-Batkaev <90351881+Khalil-Batkaev@users.noreply.github.com>
2022-04-29 12:03:12 +0300

    task_14

--- task_14.py
+++ task_14.py
@@ -4,29 +4,30 @@ n → n/2 (n is even)
 n → 3n + 1 (n is odd)
 Using the rule above and starting with 13, we generate the following sequence:
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that 
+all starting numbers finish at 1.
 Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
 
-NUMBER = 1_000_000
-max_count = 0
-elem = None
+# Для решения создаём словарь, который будет хранить длину цепочек. В алгоритм добавим проверку на уже нахождение в вычисленной цепочке. Если так, то берём результат
+
+NUMBER = 1_000_000 + 1
+result = {}
 
 for num in range(NUMBER):
-    spam = []
-    eggs = num
+    spam = num
+    count = 0
     
     while num > 1:
-        if num % 2:
-            num = 3 * num + 1
-        else:
-            num /= 2
+        if num in result:
+            count += result[num]
+            break
             
-        spam.append(num)
+        num = 3 * num + 1 if num % 2 else num / 2
         
-    if len(spam) > max_count:
-        max_count = len(spam)
-        elem = eggs
+        count += 1
+                   
+    result[spam] = count
         
-print(elem)
+print(max(result, key=result.get))
